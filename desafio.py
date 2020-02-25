59d074e1c30d350112baa0d00ece44ab58e800c4
ACBozzi <annac.bozzi@gmail.com>
2020-02-25 19:08:01 -0300

    exercício

---
+++ desafio.py
@@ -0,0 +1,24 @@
+#				DESAFIO
+#Vamos conhecer agora as especificações do nosso joguinho de adivinhação de
+#números.
+#As regras são bastante simples. Vamos a elas:
+#- O jogador deverá escolher um número inteiro qualquer entre 0 e 50;
+#- O jogador receberá dicas se seu chute está acima ou abaixo do valor oculto
+#gerado pelo computador de maneira aleatória, caso não acerte de primeira o
+#número;
+#- O jogador terá 10 tentativas para adivinhar qual foi o número oculto gerado
+#pelo computador entre o intervalo (0 a 50).
+#- Informe ao jogador o nº de tentativas feitas e quantas tentativas ainda restam
+#para ele.
+#Agora é importante que você tente fazer um algoritmo com Python no Colab para o
+#joguinho descrito acima. Com certeza você possui todos os requisitos mínimos
+#necessários para desenvolver esse algoritmo.
+#Aqui vão algumas dicas para você, caso precise. Utilize as seguintes funções:
+#- A biblioteca ‘numpy’ para gerar números aleatórios inteiros com a função
+#np.ramdom.ramdint() {estude o help da função: ?np.ramdom.ramdint()};
+#- input() : para pedir que o usuário digite um nº qualquer;
+#- Você vai precisar de alguma estrutura de controle de laço de repetição como o loop
+#While (com break – temos uma aula específica sobre esse comando);
+#- Certamente, você irá precisar das estruturas condicionais: if, elif e else;
+#- Utilize quantas vezes for necessárias a função print() para fornecer as informações do
+#andamento das tentativas ao jogador. 
